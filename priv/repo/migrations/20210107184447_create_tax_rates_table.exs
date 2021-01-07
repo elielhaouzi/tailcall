ead27591553ed6da90fdd706cce71f9370fa5dd7
Eliel Haouzi <eliel.haouzi@gmail.com>
2021-01-07 20:59:40 +0200

    add tax_rate migration

---
+++ priv/repo/migrations/20210107184447_create_tax_rates_table.exs
@@ -0,0 +1,28 @@
+defmodule Billing.Repo.Migrations.CreateTaxRatesTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:tax_rates) do
+      add(:user_id, :bigint, null: false)
+
+      add(:active, :boolean, null: false)
+      add(:created_at, :utc_datetime, null: false)
+      add(:description, :string, null: true)
+      add(:display_name, :string, null: false)
+      add(:inclusive, :boolean, null: false)
+      add(:jurisdiction, :string, null: true)
+      add(:livemode, :boolean, null: false)
+      add(:metadata, :map, null: true)
+      add(:percentage, :decimal, null: false)
+
+      add(:deleted_at, :utc_datetime, null: true)
+      timestamps()
+      add(:object, :string, default: "tax_rate")
+    end
+
+    create(index(:tax_rates, [:user_id]))
+    create(index(:tax_rates, [:active]))
+    create(index(:tax_rates, [:livemode]))
+    create(index(:tax_rates, [:deleted_at]))
+  end
+end
