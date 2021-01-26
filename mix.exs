d861a774812a620c4f34f3f14e870e3a1b168281
Eliel Haouzi <eliel.haouzi@gmail.com>
2021-01-26 16:39:04 +0200

    update annacl mix reference

--- mix.exs
+++ mix.exs
@@ -51,7 +51,7 @@ defmodule Tailcall.MixProject do
 
       #
       {:hammox, "~> 0.3", only: :test},
-      {:annacl, git: "git@github.com:annatel/annacl.git", branch: "v2"},
+      {:annacl, git: "git@github.com:annatel/annacl.git", tag: "2.0.0"},
       {:antl_utils_elixir, "~> 0.2.0"},
       {:antl_utils_ecto, "~> 1.2.1", override: true},
       {:shortcode, "~> 0.5.0"},
